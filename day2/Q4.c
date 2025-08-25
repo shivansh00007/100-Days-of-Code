8681cb26bba7bf20ee95b57c108d07479cd2b5e0
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2025-08-25 16:48:45 +0530

    Added Q4.c(Finding Area & Circumference of Circle)

--- day2/Q4.c
+++ day2/Q4.c
@@ -6,6 +6,6 @@ int main(){
             scanf("%f",&Radius);
 
             printf("The Area of Circle %f\n",3.14*Radius*Radius);
-            printf("The Circumference  of Circle %f",2*3.14*Radius);
+            printf("The Circumference of Circle %f",2*3.14*Radius);
             return 0;
 }
