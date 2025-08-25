fbd784d438f2341cf5adcdccfb5b58cfdcfd727f
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2025-08-25 15:35:55 +0530

    Added Q1.c (sum of two numbers) Added Q1.c (sum of two numbers)

--- Q1.c
+++ Q1.c
@@ -1,9 +1,14 @@
-#include<stdio.h>
-int main(){
-int num1;
-int num2;
-scanf("%d",&num1);
-scanf("%d",&num2);
-printf("The addition is %d",num1+num2);
-return 0;
+#include <stdio.h>
+int main()
+{
+            int number1, number2;
+
+            printf("Enter number1\n");
+            scanf("%d", &number1);
+
+            printf("Enter number2\n");
+            scanf("%d", &number2);
+            
+            printf("The addition is %d", number1 + number2);
+            return 0;
 }
