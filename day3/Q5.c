09e6d68788d3da92c48827050962e7103584907f
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2025-08-25 17:00:42 +0530

    Added Q5.c(Temperature Celsius to Fahrenheit)

---
+++ day3/Q5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+            int temperature;
+            printf("Enter The temperature in Celsius \n");
+            scanf("%d",&temperature);
+            printf("The Temperaure in Fahrenheit %d",temperature+32);
+            return 0;
+}
