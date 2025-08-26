9c356c3eef4de262d0339e49ed63dba064062f8d
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2025-08-26 23:27:30 +0530

    Q12.c Added("Negative or Positive")

---
+++ day6/Q12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+            int b;
+            printf("Enter The Number");
+            scanf("%d",&b);
+
+            if(b<=0){
+                        printf("%d is Negative",b);
+     
+            }
+            else{
+                        printf("%d is Positive",b);
+            }
+            return 0;
+}
