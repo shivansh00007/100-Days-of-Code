3f8569a072dbf04834541c140fbaf3ca0e4fe610
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2025-08-29 10:13:56 +0530

    Added Q14.c(Vovel and Alphabet difference)

---
+++ day7/Q14.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+            char Alphabet;
+            printf("Enter The Alphabet\n");
+            scanf("%c",&Alphabet);
+            
+            if(Alphabet=='a'){
+                        printf("Its a Vovel ",Alphabet);
+
+            }
+            else if(Alphabet=='e'){
+                        printf("Its a Vovel ",Alphabet);
+
+            }
+            else if(Alphabet=='i'){
+                        printf("Its a Vovel ",Alphabet);
+            }
+            else if(Alphabet=='o'){
+                        printf("Its a Vovel ",Alphabet);
+            }
+            else if(Alphabet=='u'){
+            
+                        printf("Its a Vovel ",Alphabet);
+
+            }
+            else{
+                        printf("Its a Consonent",Alphabet);
+            }
+            return 0;
+}
