d7496c5280b9d51759709fd38b312ba482bbeef2
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2025-09-22 10:34:59 +0530

    Added Q4

--- day2/Q4.c
+++ day2/Q4.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 int main(){
-            float Radius;
+            float Radius,Area,Circumference;
             printf("Enter The Radius\n");
             scanf("%f",&Radius);
 
-            printf("The Area of Circle %f\n",3.14*Radius*Radius);
-            printf("The Circumference of Circle %f",2*3.14*Radius);
+            Area=3.14*Radius*Radius;
+            Circumference=2*3.14*Radius;
+
+            printf("The Area of Circle %f\n",Area);
+            printf("The Circumference of Circle %f",Circumference);
             return 0;
 }
