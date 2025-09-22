56dc2e659fd838d9936400443b8204e08b1e0b78
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2025-09-22 10:08:46 +0530

    Added Q1

---
+++ day1/Q1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+            int number1, number2;
+
+            printf("Enter number1\n");
+            scanf("%d", &number1);
+
+            printf("Enter number2\n");
+            scanf("%d", &number2);
+            
+            printf("The addition is %d", number1 + number2);
+            return 0;
+}
