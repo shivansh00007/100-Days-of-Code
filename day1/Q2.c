749cd570302e53e4d5cf7899c1e7ef05a4cbb05a
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2025-09-22 10:18:14 +0530

    Refactor code structure for improved readability and maintainability

---
+++ day1/Q2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+            int number1,number2;
+            printf("Enter number1\n");
+            scanf("%d", &number1);
+
+            printf("Enter Number2\n");
+            scanf("%d",&number2);
+
+            printf("The Addition is %d \n",number1+number2);
+            printf("The Difference is %d \n",number1-number2);
+            printf("The Product is %d \n",number1*number2);
+            return 0;
+}
