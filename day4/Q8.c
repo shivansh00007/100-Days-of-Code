8a02f7fc1774ad227d02c0b21913bce0a657c396
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2025-09-22 10:24:55 +0530

    Added Q8

---
+++ day4/Q8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+            int a,b;
+            printf("Enter The Number\n");
+            scanf("%d",&b);
+
+            a=(b*(b+1))/2;
+
+            printf("Sum Obtained of first N Natural Numbers : %d\n", a,b );
+
+            return 0;
+}
