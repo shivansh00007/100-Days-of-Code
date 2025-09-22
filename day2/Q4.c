b46a493980d3a3c8f29847220d5af21385b1521a
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2025-09-22 10:20:30 +0530

    Added Q4

---
+++ day2/Q4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+            float Radius;
+            printf("Enter The Radius\n");
+            scanf("%f",&Radius);
+
+            printf("The Area of Circle %f\n",3.14*Radius*Radius);
+            printf("The Circumference of Circle %f",2*3.14*Radius);
+            return 0;
+}
