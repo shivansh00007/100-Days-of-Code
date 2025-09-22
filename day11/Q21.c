19cbb666f30163d32687471d9ac0c51d240d32d6
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2025-09-22 10:42:37 +0530

    Added Q21

---
+++ day11/Q21.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int main(){
+            int Months;
+            printf("Enter the Number(1-12)\n");
+            scanf("%d",&Months);
+
+            switch(Months){
+                        case 1:
+                        printf("Monday ,31 Days");
+                        break;
+
+                        case 2:
+                        printf("Feburary ,28 Days");
+                        break;
+
+                        case 3:
+                        printf("March ,31 Days");
+                        break;
+
+                        case 4:
+                        printf("April ,30 Days");
+                        break;
+
+                        case 5:
+                        printf("May ,31 Days");
+                        break;
+
+                        case 6:
+                        printf("June ,30 Days");
+                        break;
+
+                        case 7:
+                        printf("July ,31 Days");
+                        break;
+
+                        case 8:
+                        printf("August ,31 Days");
+                        break;
+
+                        case 9:
+                        printf("September ,30 Days");
+                        break;
+
+                        case 10:
+                        printf("October ,31 Days");
+                        break;
+
+                        case 11:
+                        printf("November ,30 Days");
+                        break;
+
+                        case 12:
+                        printf("December ,31 Days");
+                        break;
+
+                        default:
+                        printf("Invalid Number");
+            }
+            return 0;
+}
