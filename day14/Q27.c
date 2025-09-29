d38ca49d58e8041ca089030f2ace0a54836c9fb7
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2025-09-29 15:14:04 +0530

    Added Q27

---
+++ day14/Q27.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+            int i,n,c;
+            printf("Enter the value of n:\n ");
+            scanf("%d",&n);
+            for(i=0;i<=n;i++){
+                       c=(i*i);
+            }
+            printf("The sum of First %d Odd numbers is %d \n",n,c);
+            return 0;
+}
