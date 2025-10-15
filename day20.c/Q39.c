8a0c4768e2a1fc1a0183d030eea753e7e6fd2e0f
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2025-10-15 18:22:53 +0530

    100 Days Of Code

---
+++ day20.c/Q39.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+            int a;
+            printf("Enter the Number\n");
+            scanf("%d",&a);
+            printf("The Number is %d",a);
+            return 0;
+}
