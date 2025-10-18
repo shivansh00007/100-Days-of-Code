ba082d3917697c389dea7a50a3b4606526de537d
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2025-10-18 19:14:38 +0530

    100 Days Of Code

---
+++ day21/Q42.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+            int a,b;
+            printf("Enter the Number\n");
+            scanf("%d",&a);
+
+            printf("enter th Number\n");
+            scanf("%d",&b);
+
+            int product=a*b;
+            printf("%d",product);
+            return 0;
+}
