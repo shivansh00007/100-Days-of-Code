fd2d90fd213778ebd92b3e7159e0adfc298d1fbc
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2025-11-01 23:34:40 +0530

    100 Days Of Code

---
+++ day26/Q52.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+            for (int i = 0; i < 4; i++)
+            {
+                        printf("*\n");
+            }
+            printf("\n");
+
+            for (int i = 0; i < 5; i++)
+            {
+                        printf("*\n");
+            }
+            printf("\n");
+
+            for (int i = 0; i < 3; i++)
+            {
+                        printf("*\n");
+            }
+            printf("\n");
+
+            printf("*\n");
+
+            return 0;
+}
