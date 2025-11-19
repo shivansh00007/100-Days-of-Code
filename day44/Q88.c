96e477752bc8875ad5d67c27db7a4a737b9b3352
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2025-11-19 23:53:01 +0530

    100 Days Of Code

--- day44/Q88.c
+++ day44/Q88.c
@@ -1,1 +1,19 @@
 //Replace spaces with hyphens in a string.
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int i = 0;
+
+    scanf("%[^\n]", str);
+
+    while (str[i] != '\0') {
+        if (str[i] == ' ')
+            str[i] = '-';
+        i++;
+    }
+
+    printf("%s", str);
+
+    return 0;
+}
