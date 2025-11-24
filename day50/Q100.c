188b765b2c5b99dbaf92333db3f344357c135481
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2025-11-24 23:42:31 +0530

    100 Days Of Code

---
+++ day50/Q100.c
@@ -0,0 +1,24 @@
+//Print all sub-strings of a string.
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str); 
+    int n = strlen(str); 
+    for(int i = 0; i < n; i++) 
+    {
+        
+        for(int j = i; j < n; j++) 
+        {
+            for(int k = i; k <= j; k++) 
+            {
+                printf("%c,", str[k]);
+            }
+        }
+    }
+    printf("\n");
+    return 0;
+}
